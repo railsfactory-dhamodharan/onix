d299ff3894adb37a7e82616c05d969532ef47114
James Healy <jimmy@deefa.com>
2008-08-04 12:13:27 +1000

    increment version to 0.3.0

--- lib/onix.rb
+++ lib/onix.rb
@@ -6,3 +6,13 @@ require File.dirname(__FILE__) + "/onix/base"
 require File.dirname(__FILE__) + "/onix/header"
 require File.dirname(__FILE__) + "/onix/product"
 require File.dirname(__FILE__) + "/onix/reader"
+
+module ONIX
+  module Version #:nodoc:
+    Major = 0
+    Minor = 3
+    Tiny  = 0
+
+    String = [Major, Minor, Tiny].join('.')
+  end
+end
