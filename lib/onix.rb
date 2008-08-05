660a5943ac1ec16c40fb5e5ddbf37128edca79aa
James Healy <jimmy@deefa.com>
2008-08-05 18:52:55 +1000

    Started a list of valid product forms

--- lib/onix.rb
+++ lib/onix.rb
@@ -7,6 +7,9 @@ require File.dirname(__FILE__) + "/onix/header"
 require File.dirname(__FILE__) + "/onix/product"
 require File.dirname(__FILE__) + "/onix/reader"
 
+# lists
+require File.join(File.dirname(__FILE__), "onix", "lists", "product_form")
+
 module ONIX
   module Version #:nodoc:
     Major = 0
