f35131b9f08861f9088d691e706d8d1370391cb5
tim <tim@milkfarmproductions.com>
2010-06-24 00:39:48 -0700

    Add SLProduct class, a product wrapper for US School/Library market products

---
+++ lib/onix/sl_product.rb
@@ -0,0 +1,96 @@
+# coding: utf-8
+
+module ONIX
+  class SLProduct < APAProduct
+    
+    delegate :audience_code, :audience_code=
+    delegate :copyright_year, :copyright_year=
+    delegate :product_form_detail, :product_form_detail=
+    
+    # retrieve the value of a particular ID
+    def series(str)
+      product.series.find { |id| id.title_of_series == str }
+    end
+
+    # set the value of a particular ID
+    def series=(value)
+      # process based on value type
+      if value.is_a?(Series)
+        str = value.title_of_series
+        series_id = value
+      else
+        str = value
+        series_id = ONIX::Series.new
+        series_id.title_of_series = value
+      end
+      # check if exists already
+      unless series(str)
+        product.series << series_id
+      end
+    end
+    
+    # retrieve an array of all languages
+    def languages
+      product.languages.collect { |language| language.language_code}
+    end
+
+    # set a new language to this product
+    # str should be the languages name code (US)
+    def add_language(str, role = "1", country = "US")
+      language = ::ONIX::Language.new
+      language.language_role = role
+      language.language_code = str
+      language.country_code = country
+      product.languages << language
+    end
+    
+    # retrieve the dewey for this title
+    def dewey
+      product.subjects.find { |sub| sub.subject_scheme_id.to_i == 1 } # dewey = 1, abridged dewey = 2
+    end
+
+    # add a dewey to the product
+    def dewey=(code)
+      add_subject code, "1" # dewey = 1, abridged dewey = 2
+    end
+    
+    # audience_range_qualifier: 11 = US school grades
+    # audience_range_precision: 1 = exact, 3 = from, 4 = to
+    def us_grade_range
+      audience_range(11)
+    end
+
+    # set a new "audience of text" (role=1) country code for this particular product
+    def us_grade_range=(range=[])
+      audience_range_set(11, range[0], range[1])
+    end
+
+    # retrieve the value of a particular audience range
+    def audience_range(type)
+      product.audience_ranges.find { |a| a.audience_range_qualifier == type }
+    end
+
+    # set the value of a particular audience_range
+    def audience_range_set(type, min=nil, max=nil)
+      a = audience_range(type)
+
+      # create a new audience_range record if we need to
+      if a.nil?
+        a = ONIX::AudienceRange.new
+        a.audience_range_qualifier = type
+        product.audience_ranges << a
+      end
+
+      unless min.nil?
+        a.audience_range_precisions << "03"
+        a.audience_range_values << min
+      end
+      unless max.nil?
+        a.audience_range_precisions << "04"
+        a.audience_range_values << max
+      end
+      a.audience_range_precisions = ["01"] if min.nil? || max.nil?
+    end
+    
+  end
+end
---
+++ spec/sl_product_spec.rb
@@ -0,0 +1,99 @@
+# coding: utf-8
+
+require File.dirname(__FILE__) + '/spec_helper.rb'
+require 'date'
+
+context "ONIX::SLProduct" do
+
+  before(:each) do
+    @data_path = File.join(File.dirname(__FILE__),"..","data")
+    file1    = File.join(@data_path, "product.xml")
+    @doc     = Nokogiri::XML::Document.parse(File.read(file1))
+    @product_node = @doc.root
+  end
+
+  specify "should provide read access to attibutes" do
+    @product = ONIX::Product.from_xml(@product_node.to_s)
+    @sl     = ONIX::SLProduct.new(@product)
+
+    @sl.record_reference.should eql("365-9780194351898")
+    @sl.notification_type.should eql(3)
+    @sl.product_form.should eql("BC")
+    @sl.number_of_pages.should eql(100)
+    @sl.bic_main_subject.should eql("EB")
+    @sl.publishing_status.should eql(4)
+    @sl.publication_date.should eql(Date.civil(1998,9,1))
+    @sl.pack_quantity.should eql(12)
+  end
+
+  specify "should provide write access to attibutes" do
+    sl = ONIX::SLProduct.new
+
+    sl.notification_type = 3
+    sl.to_xml.to_s.include?("<NotificationType>03</NotificationType>").should be_true
+
+    sl.record_reference = "365-9780194351898"
+    sl.to_xml.to_s.include?("<RecordReference>365-9780194351898</RecordReference>").should be_true
+
+    sl.product_form = "BC"
+    sl.to_xml.to_s.include?("<ProductForm>BC</ProductForm>").should be_true
+
+    sl.number_of_pages = 100
+    sl.to_xml.to_s.include?("<NumberOfPages>100</NumberOfPages>").should be_true
+
+    sl.bic_main_subject = "EB"
+    sl.to_xml.to_s.include?("<BICMainSubject>EB</BICMainSubject>").should be_true
+
+    sl.publishing_status = 4
+    sl.to_xml.to_s.include?("<PublishingStatus>04</PublishingStatus>").should be_true
+
+    sl.publication_date = Date.civil(1998,9,1)
+    sl.to_xml.to_s.include?("<PublicationDate>19980901</PublicationDate>").should be_true
+
+    sl.pack_quantity = 12
+    sl.to_xml.to_s.include?("<PackQuantity>12</PackQuantity>").should be_true
+  end
+
+end
+
+context ONIX::SLProduct, "series method" do
+  specify "should set the nested series value on the underlying product class" do
+    sl = ONIX::SLProduct.new
+
+    sl.series = "Harry Potter"
+    sl.series.should eql("Harry Potter")
+    sl.to_xml.to_s.include?("<TitleOfSeries>Harry Potter</TitleOfSeries>").should be_true
+  end
+end
+
+context ONIX::SLProduct, "price method" do
+  before(:each) do
+    @data_path = File.join(File.dirname(__FILE__),"..","data")
+    file1    = File.join(@data_path, "usd.xml")
+    @doc     = Nokogiri::XML::Document.parse(File.read(file1))
+    @product_node = @doc.root
+  end
+
+  specify "should return the first price in the file, regardless of type" do
+    @product = ONIX::Product.from_xml(@product_node.to_s)
+    @sl     = ONIX::SLProduct.new(@product)
+
+    @sl.price.should eql(BigDecimal.new("99.95"))
+  end
+end
+
+context ONIX::SLProduct, "rrp_exc_sales_tax method" do
+  before(:each) do
+    @data_path = File.join(File.dirname(__FILE__),"..","data")
+    file1    = File.join(@data_path, "usd.xml")
+    @doc     = Nokogiri::XML::Document.parse(File.read(file1))
+    @product_node = @doc.root
+  end
+
+  specify "should return the first price in the file of type 1" do
+    @product = ONIX::Product.from_xml(@product_node.to_s)
+    @sl     = ONIX::SLProduct.new(@product)
+
+    @sl.rrp_exc_sales_tax.should eql(BigDecimal.new("99.95"))
+  end
+end
