dd8e0e7d1188119863207121611d7f82e4586d1a
tim <tim@milkfarmproductions.com>
2011-03-24 14:36:47 -0700

    Update rspec test syntax, set_spec.rb

--- spec/set_spec.rb
+++ spec/set_spec.rb
@@ -2,7 +2,7 @@
 
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-context "ONIX::Set" do
+describe ONIX::Set do
 
   before(:each) do
     data_path = File.join(File.dirname(__FILE__),"..","data")
@@ -11,18 +11,18 @@ context "ONIX::Set" do
     @root = @doc.root
   end
 
-  specify "should correctly convert to a string" do
+  it "should correctly convert to a string" do
     set = ONIX::Set.from_xml(@root.to_s)
     set.to_xml.to_s[0,5].should eql("<Set>")
   end
 
-  specify "should provide read access to first level attributes" do
+  it "should provide read access to first level attributes" do
     set = ONIX::Set.from_xml(@root.to_s)
 
     set.title_of_set.should eql("Citizens and Their Governments")
   end
 
-  specify "should provide write access to first level attributes" do
+  it "should provide write access to first level attributes" do
     set = ONIX::Set.new
 
     set.title_of_set = "Cool Science Careers"
