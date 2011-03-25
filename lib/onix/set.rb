3aff6edaba8b9c5fea281bb2add8398dc59e3ff4
tim <tim@milkfarmproductions.com>
2011-03-24 17:34:52 -0700

    Improve Set composite backwards compatibility

--- lib/onix/set.rb
+++ lib/onix/set.rb
@@ -15,10 +15,12 @@ module ONIX
       self.initialize_product_identifiers(options) # Must be called to setup @product_identifiers array
     end
     
-    # Following methods removed os of gem 0.8.9 (not backwards compatible):
+    # Following methods removed os of gem 0.8.9 (aliased for backwards compatible):
+    alias_method :proprietary_set_id, :proprietary_id
+    alias_method :proprietary_set_id=, :proprietary_id=
+    
+    # Following methods removed os of gem 0.8.9 (NOT backwards compatible):
     #   original method -> replacement method
-    #   proprietary_set_id -> proprietary_id
-    #   proprietary_set_id=(id) -> proprietary_id=(id)
     #   product_identifier(type) -> see product_identifiers.rb (eg, use isbn, lccn...)
     #   product_identifier_set(type, value) -> see product_identifiers.rb (eg, use isbn=, lccn=...)
     
